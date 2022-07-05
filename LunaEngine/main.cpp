c8df0609511c0a57a85fe91e2bf792a0e30e106c
Cppshizoid <alex.krivoschecov@gmail.com>
2022-07-06 00:13:55 +0700

    Create main.cpp

---
+++ LunaEngine/main.cpp
@@ -0,0 +1,24 @@
+#include <iApplication.h>
+#include <Steam/SteamModule.h>
+
+int main(void)
+{
+	// Initialize Steam
+	InitSteam();
+
+	if (iApplication::_instance)
+	{
+		iApplication::_instance->Init();
+		iApplication::_instance->Run();
+		iApplication::_instance->End();
+	}
+	else
+	{
+		std::cout << "No application found..." << std::endl;
+	}
+
+	// Release Steam
+	ReleaseSteam();
+
+	return 0;
+}
