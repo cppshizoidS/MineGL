09c380d546f9c09a0decceb51ff0e292d0a59178
Cppshizoid <alex.krivoschecov@gmail.com>
2022-07-07 04:36:45 +0700

    Create GLFunctions.cpp

---
+++ MineClone/MineClone/src/GL/GLFunctions.cpp
@@ -0,0 +1,16 @@
+#include "GLFunctions.h"
+
+void GL::drawElements(GLuint indicesCount) noexcept
+{
+    glDrawElements(GL_TRIANGLES, indicesCount, GL_UNSIGNED_INT, nullptr);
+}
+
+void GL::drawLines(GLuint indicesCount) noexcept
+{
+	glDrawElements(GL_LINES, indicesCount, GL_UNSIGNED_INT, nullptr);
+}
+
+void GL::bindVAO(GLuint vao) noexcept
+{
+    glBindVertexArray(vao);
+}
