c62897ca093db649548021357d17d3b5b3d2591b
Cppshizoid <alex.krivoschecov@gmail.com>
2022-07-08 00:20:28 +0700

    Add files via upload

---
+++ MineClone/MineClone/src/Audio/Sound/SoundId.h
@@ -0,0 +1,46 @@
+#pragma once
+
+enum SoundId {
+
+	StepGrass,
+	StepSand,
+	StepSnow,
+	StepGravel,
+	StepStone,
+	StepWood,
+	StepWool,
+
+	BreakGlass,
+	BreakGravel,
+	BreakSnow,
+	BreakSand,
+	BreakGrass,
+	BreakStone,
+	BreakWood,
+
+	EnterWater,
+	ExitWater,
+
+	UnderwaterCommon,
+	UnderwaterRare,
+	UnderwaterUltraRare,
+
+	DrownDamage,
+
+	RainSound,
+
+	Eat,
+
+
+
+	UnderwaterAmbience,
+	WaterSplashNormal,
+	Plop,
+	FallDamage,
+	ClassicHurt,
+	PlayerWasHit,
+	Burp,
+	ItemBreak,
+
+	NUMBER_OF_SOUNDS,
+};
