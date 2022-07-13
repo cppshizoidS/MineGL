0d4052d4d30b8d4ffc1abba87a0f2b5e2b99670b
Cppshizoid <alex.krivoschecov@gmail.com>
2022-07-13 13:19:58 +0700

    Update TerrainGeneratot.h

--- MineClone/MineClone/src/World/Generation/Terrain/TerrainGeneratot.h
+++ MineClone/MineClone/src/World/Generation/Terrain/TerrainGeneratot.h
@@ -1,1 +1,17 @@
+#pragma once
 
+#include "../Biome/Biome.h"
+
+class Chunk;
+
+//struct Biome;
+
+class TerrainGenerator {
+public:
+	virtual ~TerrainGenerator() = default;
+
+    virtual void generateTerrainFor(Chunk &chunk) = 0;
+    virtual int getMinimumSpawnHeight() const noexcept = 0;
+
+	virtual BiomeId getBiomeId(Chunk &chunk, int x, int z) = 0;
+};
